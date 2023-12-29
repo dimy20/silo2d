c6a114f37f598e5ec589c5ea61a27ace1edefab0
dimy20 <enzostrindade16@outlook.com>
2023-12-29 18:17:25 -0300

    xy grid

--- main.py
+++ main.py
@@ -22,15 +22,13 @@ def do_input():
     elif key == 32:
         alpha -= 1.0
 
-def projection(f, c):
-    cx, cy = c
-
-    mat = np.array([[f, 0, cx],
-                    [0, f, cy],
-                    [0, 0, 1]])
-    return mat
-
-def draw_grid(img):
+def perspective_devide(p : np.ndarray):
+    assert p.shape[0] == 3
+    z = p[2]
+    p /= z
+    return p
+    
+def draw_grid(img, x_grid_size = (-2, 3), z_grid_size = (5, 36)):
     f, cx, cy, L = 810.5, 480, 270, 3.31 # Unit: [px], [px], [px], [m]
 
     camera_orientation = [-18.7, -8.2, 2.0] #Euler form
@@ -49,41 +47,42 @@ def draw_grid(img):
                             [0, f, cy], 
                             [0, 0, 1]])
 
-    grid_x = (-2, 3)
-    grid_z = (5, 36)
 
-    for x in range(*grid_x):
-        p1 = np.array([x, 0, grid_z[0], 1.0])
-        p2 = np.array([x, 0, grid_z[1], 1.0])
-
+    def apply_transformations(p1, p2):
         view_p1 = np.matmul(view_matrix, p1.T)
         view_p2 = np.matmul(view_matrix, p2.T)
 
         p1_projected = np.matmul(proj_matrix, view_p1[:-1].T)
         p2_projected = np.matmul(proj_matrix, view_p2[:-1].T)
 
-        print(p1_projected)
-
         #perspective devide
-        p1_projected[0] /= p1_projected[2]
-        p1_projected[1] /= p1_projected[2]
+        p1_projected = perspective_devide(p1_projected).astype(np.int32)
+        p2_projected = perspective_devide(p2_projected).astype(np.int32)
 
-        p2_projected[0] /= p2_projected[2]
-        p2_projected[1] /= p2_projected[2]
+        return (p1_projected[0], p1_projected[1]), (p2_projected[0], p2_projected[1])
 
-        color = (64, 128, 64)
+    color = (64, 128, 64)
 
-        start = (int(p1_projected[0]), int(p1_projected[1]))
-        end = (int(p2_projected[0]), int(p2_projected[1]))
+    for x in range(*x_grid_size):
+        p1 = np.array([x, 0, z_grid_size[0], 1.0])
+        p2 = np.array([x, 0, z_grid_size[1], 1.0])
+
+        start, end = apply_transformations(p1, p2)
+        cv2.line(img, start, end, color, 2)
+
+    for z in range(*z_grid_size):
+        p1 = np.array([x_grid_size[0], 0.0, z, 1.0])
+        p2 = np.array([x_grid_size[1], 0.0, z, 1.0])
+
+        start, end = apply_transformations(p1, p2)
 
         cv2.line(img, start, end, color, 2)
 
     return img
 
 if __name__ == '__main__':
-
     img = cv2.imread(test_image)
-    img= draw_grid(img)
+    img = draw_grid(img)
 
     while running:
         cv2.imshow(WINDOW_NAME, img)
