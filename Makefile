4d17d4af31f18dfc2defdc9e1bde56fcaf244ae8
dimy20 <enzostrindade16@outlook.com>
2024-01-08 14:18:48 -0300

    adding imgui for ui

--- Makefile
+++ Makefile
@@ -5,18 +5,35 @@ SRC_DIR = ./src
 BUILD_DIR = ./build
 INCLUDE_DIR = include
 
-SRCS = $(shell find $(SRC_DIR) -name '*.cpp')
-OBJS = $(patsubst $(BUILD_DIR)/%.o, $(SRC_DIR)/%.cpp, $(SRCS))
+SRCS = $(wildcard $(SRC_DIR)/*.cpp $(wildcard) $(SRC_DIR)/Physics2D/*.cpp)
+OBJS = $(patsubst $(SRC_DIR)/%.cpp, $(BUILD_DIR)/%.o, $(SRCS))
+
+#Imgui source files and object files definitions
+IMGUI_SRCDIR = ./Imgui
+IMGUI_SRCS = $(wildcard $(IMGUI_SRCDIR)/*.cpp)
+IMGUI_OBJS = $(patsubst $(IMGUI_SRCDIR)/%.cpp, $(BUILD_DIR)/Imgui/%.o, $(IMGUI_SRCS))
 
 LIBRARIES = -ldl -lglfw
-CFLAGS = -Wall -Werror -g -std=c++20 -I$(INCLUDE_DIR)
+CFLAGS = -Wall -Werror -g -std=c++17 -I$(INCLUDE_DIR) -I$(IMGUI_SRCDIR)
 
-$(EXEC): $(OBJS)
+.PHONY: all clean
+
+all: $(EXEC)
+
+$(EXEC): $(OBJS) $(IMGUI_OBJS)
 	$(GCC) $(CFLAGS) -o $@ $^ $(LIBRARIES)
 
-$(BUILD_DIR)/%.o: %(SRC_DIR)/%.cpp
-	$(GCC) $(CFLAGS) -c $^ -o $@
+$(BUILD_DIR)/%.o: $(SRC_DIR)/%.cpp | $(BUILD_DIR)
+	$(GCC) $(CFLAGS) -c $< -o $@
+
+$(BUILD_DIR)/Imgui/%.o: $(IMGUI_SRCDIR)/%.cpp | $(BUILD_DIR)/Imgui
+	$(GCC) $(CFLAGS) -c $< -o $@
+
+$(BUILD_DIR):
+	mkdir -p $(BUILD_DIR) $(BUILD_DIR)/Physics2D
+
+$(BUILD_DIR)/Imgui:
+	mkdir -p $(BUILD_DIR)/Imgui
 
 clean:
-	rm -rf $(BUILD_DIR)/*.o $(EXEC)
-	
+	rm -rf $(BUILD_DIR)/*.o $(BUILD_DIR)/Imgui/*.o $(EXEC)
--- src/main.cpp
+++ src/main.cpp
@@ -16,6 +16,10 @@
 #include <Physics2D/Particle.h>
 #include <Physics2D/Forces.h>
 
+#include "imgui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
+
 #define WINDOW_W 800
 #define WINDOW_H 600
 #define WINDOW_NAME "Silo"
@@ -83,6 +87,8 @@ bool App::init(){
                                  Renderer::WinWidth(), 
                                  Renderer::WinHeight() / 2);
 
+
+
     return true;
 };
 
@@ -130,7 +136,7 @@ void App::update(){
 
         p.applyForce(windForce * Physics2D::PIXELS_PER_METER);
         p.applyForce(Physics2D::weightForce(p) * Physics2D::PIXELS_PER_METER);
-        p.applyForce(Physics2D::frictionForce(p, 10.0f * Physics2D::PIXELS_PER_METER));
+        //p.applyForce(Physics2D::frictionForce(p, 10.0f * Physics2D::PIXELS_PER_METER));
 
         if(mMousePos != glm::vec2(0.0f)){
             p.applyForce(glm::normalize(mMousePos - p.mPosition) * 20.0f * Physics2D::PIXELS_PER_METER);
@@ -171,12 +177,75 @@ int main(){
         exit(1);
     }
 
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+
+    ImGuiIO& io = ImGui::GetIO();
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;
+
+        // Setup Dear ImGui style
+    ImGui::StyleColorsDark();
+    //ImGui::StyleColorsLight();
+
+    // Setup Platform/Renderer backends
+    ImGui_ImplGlfw_InitForOpenGL(Renderer::getWindow(), true);
+    ImGui_ImplOpenGL3_Init("#version 450");
+
+
+    bool show_demo_window = true;
+    bool show_another_window = false;
+    ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+
     while(!Renderer::shouldClose()){
         app.doInput(Renderer::getWindow());
 
         app.update();
 
         Renderer::clear();
+
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+          // 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear ImGui!).
+        if (show_demo_window)
+            ImGui::ShowDemoWindow(&show_demo_window);
+
+        // 2. Show a simple window that we create ourselves. We use a Begin/End pair to create a named window.
+        {
+            static float f = 0.0f;
+            static int counter = 0;
+
+            ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
+
+            ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
+            ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
+            ImGui::Checkbox("Another Window", &show_another_window);
+
+            ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
+            ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
+
+            if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
+                counter++;
+            ImGui::SameLine();
+            ImGui::Text("counter = %d", counter);
+
+            ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / io.Framerate, io.Framerate);
+            ImGui::End();
+        }
+
+        // 3. Show another simple window.
+        if (show_another_window)
+        {
+            ImGui::Begin("Another Window", &show_another_window);   // Pass a pointer to our bool variable (the window will have a closing button that will clear the bool when clicked)
+            ImGui::Text("Hello from another window!");
+            if (ImGui::Button("Close Me"))
+                show_another_window = false;
+            ImGui::End();
+        }
+
+        ImGui::Render();
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 
         //Render
         app.draw();
